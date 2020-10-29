1fbdbe57a30512fed5b61a1909488dff10a162b0
Jayx20 <Jayx2002@gmail.com>
2020-10-29 16:33:28 -0500

    Setup the Drive Class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -1,6 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class Drive {
 
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor backLeft;
+    DcMotor backRight;
+
+    Robot opMode;
+
+    void init (Robot opMode) {
+        this.opMode = opMode;
+
+        frontLeft = opMode.hardwareMap.get(DcMotor.class,"frontLeft");
+        frontRight = opMode.hardwareMap.get(DcMotor.class,"frontRight");
+        backLeft = opMode.hardwareMap.get(DcMotor.class,"backLeft");
+        backRight = opMode.hardwareMap.get(DcMotor.class,"backRight");
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    }
 
 }
