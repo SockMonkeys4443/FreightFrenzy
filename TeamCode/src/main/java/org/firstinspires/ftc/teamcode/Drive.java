05091b70d07c0c763ff6125c834fed3a37b55f31
Jayx20 <Jayx2002@gmail.com>
2020-11-03 16:04:50 -0600

    added most of the base drive methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -26,13 +26,59 @@ public class Drive {
 
     }
 
+    //Directional Movement
     public void driveForward(double power) {
-        frontLeft.setPower(power);
-        frontRight.setPower(power);
-        backLeft.setPower(power);
-        backRight.setPower(power);
+        setMotorPower(power, power, power, power);
+    }
+
+    public void driveBackward(double power) {
+        setMotorPower(-power, -power, -power, -power);
+    }
+
+    public void strafeLeft(double power) {
+        setMotorPower(-power, power, power, -power);
+    }
+
+    public void strafeRight(double power) {
+        setMotorPower(power, -power, -power, power);
     }
 
 
+    //Angular Movement
+    public void strafeForwardLeft(double power) {
+        setMotorPower(0, power, power, 0);
+    }
+
+    public void strafeForwardRight(double power) {
+        setMotorPower(power, 0, 0, power);
+    }
+
+    public void strafeBackwardLeft(double power) {
+        setMotorPower(-power, 0, 0, -power);
+    }
+
+    public void strafeBackwardRight(double power) {
+        setMotorPower(0, -power, -power, 0);
+    }
+
+
+    //turning
+    public void turnLeft(double power) {
+        setMotorPower(-power, power, -power, power);
+    }
+
+    public void turnReft(double power) {
+        setMotorPower(power, -power, power, -power);
+    }
+
+
+
+
+    private void setMotorPower(double frontLeft, double frontRight, double backLeft, double backRight) {
+        this.frontLeft.setPower(frontLeft);
+        this.frontRight.setPower(frontRight);
+        this.backLeft.setPower(backLeft);
+        this.backRight.setPower(backRight);
+    }
 
 }
