148ff03d19cbb922f7ff265f247e2fdc6551d8ee
SockMonkeys4443 <sockmonkeys4443@gmail.com>
2021-11-11 17:20:54 -0600

    bruh bot is now yogurt male

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BruhBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BruhBot.java
@@ -4,8 +4,10 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
+
 
 
 @TeleOp(name = "BRUH BOT BRUH BOT", group = "working")
@@ -25,7 +27,23 @@ public class BruhBot extends LinearOpMode {
 
     private DcMotor duckyWheel;
 
-    double brazoSpeed = 0.2;
+    private Servo scoop;
+
+    private DigitalChannel brazoLimit;
+
+    double wheelSpeed = 1;
+
+    double duckyPower = 0;
+
+    double brazoSpeed = 0.4;
+
+    double scoopPosition = 0;
+    double scoopRotationModifier = 0.035;
+
+    boolean y1Released = true;
+    boolean a1Released = true;
+    boolean y2Released = true;
+
 
     @Override
     public void runOpMode() {
@@ -45,26 +63,64 @@ public class BruhBot extends LinearOpMode {
 
         duckyWheel = hardwareMap.get(DcMotor.class, "ducky_wheel");
 
+        scoop = hardwareMap.get(Servo.class, "scoop");
+
+        brazoLimit = hardwareMap.get(DigitalChannel.class, "brazo_limit");
+
         frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
         backRightDrive.setDirection(DcMotor.Direction.REVERSE);
 
+        brazo.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        brazo.setDirection(DcMotor.Direction.REVERSE);
+
         waitForStart();
         runtime.reset();
+
 
 
         while (opModeIsActive()) {
             driveRobot();
             runBrazo();
 
+            rotateScoop();
+
             setIntakePower(gamepad2.right_trigger - gamepad2.left_trigger);
+
+            if (gamepad2.b) {
+                scoopPosition = 1;
+            } else if (gamepad2.a) {
+                scoopPosition = 0.5;
+            } else if (gamepad2.x) {
+                scoopPosition = 0;
+            }
+
+
+
+            if (gamepad1.y && y1Released) {
+                duckyPower = duckyPower == 1 ? 0 : 1;
+                y1Released = false;
+            } else if (!gamepad1.y && !y1Released) {
+                y1Released = true;
+            }
+
+            if (gamepad1.a && a1Released) {
+                wheelSpeed = wheelSpeed == 1 ? 0.4 : 1;
+                a1Released = false;
+            } else if (!gamepad1.a && !a1Released) {
+                a1Released = true;
+            }
+
+            scoop.setPosition(scoopPosition);
+
+            duckyWheel.setPower(duckyPower);
 
             idle();
         }
     }
 
     private void driveRobot() {
-        double forwardPower = gamepad1.left_stick_y;
-        double turnPower = gamepad1.right_stick_x * 0.7;
+        double forwardPower = gamepad1.left_stick_y * wheelSpeed;
+        double turnPower = gamepad1.right_stick_x * 0.85 * wheelSpeed;
 
         frontLeftDrive.setPower(forwardPower + turnPower);
         frontRightDrive.setPower(forwardPower - turnPower);
@@ -73,7 +129,7 @@ public class BruhBot extends LinearOpMode {
     }
 
     private void runBrazo() {
-        double brazoPower = gamepad2.right_stick_y * brazoSpeed;
+        double brazoPower = gamepad2.left_stick_y * brazoSpeed;
 
         brazo.setPower(brazoPower);
     }
@@ -81,6 +137,12 @@ public class BruhBot extends LinearOpMode {
     private void setIntakePower(double power) {
         leftIntake.setPower(power);
         rightIntake.setPower(power);
+    }
+
+    private void rotateScoop() {
+        scoopPosition += gamepad2.right_stick_y * scoopRotationModifier;
+
+        //scoop.setPosition(scoopPosition);
     }
 
 
